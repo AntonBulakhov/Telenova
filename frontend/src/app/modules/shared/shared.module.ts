32bda799de259fc7873cb0cf1f8b145437b6dfd9
Anton <frostanton09@gmail.com>
2019-10-31 07:04:54 +0300

    Home page completed

--- frontend/src/app/modules/shared/shared.module.ts
+++ frontend/src/app/modules/shared/shared.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { CardComponent } from './card/card.component';
 
 
 
 @NgModule({
   declarations: [
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    CardComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    CardComponent
   ]
 })
 export class SharedModule { }
