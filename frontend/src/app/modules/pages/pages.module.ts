ffa78e97d8c1010ddf05550310f367c04af41336
Anton <frostanton09@gmail.com>
2019-11-01 16:27:47 +0300

    Registration page

--- frontend/src/app/modules/pages/pages.module.ts
+++ frontend/src/app/modules/pages/pages.module.ts
@@ -5,11 +5,12 @@ import {SharedModule} from "../shared/shared.module";
 import {BrowserModule} from "@angular/platform-browser";
 import {RouterModule} from "@angular/router";
 import { ProfileComponent } from './profile/profile.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 
 
 @NgModule({
-  declarations: [HomeComponent, ProfileComponent],
+  declarations: [HomeComponent, ProfileComponent, RegistrationComponent],
   imports: [
     CommonModule,
     SharedModule,
@@ -18,7 +19,8 @@ import { ProfileComponent } from './profile/profile.component';
   ],
   exports: [
     HomeComponent,
-    ProfileComponent
+    ProfileComponent,
+    RegistrationComponent
   ]
 })
 export class PagesModule { }
