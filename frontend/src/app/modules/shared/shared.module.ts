ee66d9848d5b1d5a7b14b287a65e0d44e6b386aa
Anton <frostanton09@gmail.com>
2019-11-01 15:59:54 +0300

    FrontEnd changes

--- frontend/src/app/modules/shared/shared.module.ts
+++ frontend/src/app/modules/shared/shared.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { CardComponent } from './card/card.component';
+import {RouterModule} from "@angular/router";
 
 
 
@@ -13,7 +14,8 @@ import { CardComponent } from './card/card.component';
     CardComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     NavbarComponent,
